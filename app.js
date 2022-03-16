0bbbf05af0ab2cfe898e04e998f460fc3b55dc91
Jason <jasonau003@gmail.com>
2022-03-15 20:42:04 -0400

    boring html + update js

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-alert('work')
+const updateDisplay = (numbers) => {
+    const display = document.querySelector('.display');
+    display.textContent = display.textContent + numbers
+}
+
+const operate = (a, b, operator) => {
+    if (operator === "+") {
+        add(a,b)
+    } else if (operator === "-") {
+        subtract(a,b)
+    } else if (operator === "*") {
+        multiply(a,b)
+    } else if (operator === "/") {
+        divide(a,b)
+    }
+}
+
+const add = (a,b) => {
+	return a+b;
+};
+
+const subtract = (a,b) => {
+	return a-b;
+};
+
+const multiply = (a,b) => {
+	return a*b;
+};
+
+const divide = (a,b) => {
+	return a/b;
+};
+
+const numberBtn = document.querySelectorAll(['[data-number]'])
+
+numberBtn.forEach(btn => btn.addEventListener("click", () => {
+    updateDisplay(btn.textContent)
+}))
